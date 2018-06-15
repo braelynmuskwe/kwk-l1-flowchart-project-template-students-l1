e99edfc0a7b832d4931879dba79fe63e91e44e25
braelynmuskwe <braemira1@icloud.com>
2018-06-15 17:22:21 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
-class ApplicationController < Sinatra::Base
+# class ApplicationController < Sinatra::Base
 
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
+#   configure do
+#     set :public_folder, 'public'
+#     set :views, 'app/views'
+#   end
+  
+#   get "/" do
+#     erb :index
+#   end 
 
-end
+# end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,3 +1,24 @@
-class Quiz
-
-end
+# def check(occasion, weather, gender)
+#   answers= {
+#     :question_1 => 
+#     :question_2 =>
+#     :question_3 =>
+#   }
+  
+#   puts "Is your occasion formal or casual?"
+# occasion=gets.chomp.upcase
+# end
+# def weather 
+#   puts "Is the weather forecast of the event hot, cold, or warm?"
+#   weather=gets.chomp.upcase
+# end
+# def gender
+#   puts "Do you prefer feminine or masculine clothing?"
+#   gender=gets.chomp.upcase
+# end
+# occasion
+# weather
+# gender
+# def outfits
+#   if occasion == "formal" & weather == "hot" gender == "feminine"
+#     then outfit B == occassion
